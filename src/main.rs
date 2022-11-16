6cf05dc4c847429c21ca758e08a3115725862ab8
Tyler Mathis <tmathis204@gmail.com>
2022-11-16 12:00:27 +0100

    first working tool

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![allow(unused)]
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file '{}'", args.path.display()))?;
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
